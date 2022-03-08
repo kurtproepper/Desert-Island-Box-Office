7ae593d9aa0bf024d15958546591b454e14705d3
Kurt Proepper <nick.k.proepper@gmail.com>
2022-03-08 17:39:05 -0600

    Initial front end

---
+++ desert-island-box-office/src/views/Home.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  data: function () {
+    return {
+      message: "Desert Island Box Office",
+    };
+  },
+  created: function () {},
+  methods: {},
+};
+</script>
+
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<style></style>
