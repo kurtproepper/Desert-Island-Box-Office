1c5a9458cb77ad65f1e281a336c3b80063a96abf
Kurt Proepper <nick.k.proepper@gmail.com>
2022-03-17 20:54:14 -0500

    Used v-for and bind to display only titles on the profile page

--- app/serializers/user_movie_serializer.rb
+++ app/serializers/user_movie_serializer.rb
@@ -1,3 +1,4 @@
 class UserMovieSerializer < ActiveModel::Serializer
   attributes :id, :movie_id, :user_id
+  belongs_to :movie
 end
--- desert-island-box-office/src/views/Profile.vue
+++ desert-island-box-office/src/views/Profile.vue
@@ -21,26 +21,34 @@ export default {
     movieSearch: function () {
       axios.get("/movies/search/" + this.searchText).then((response) => {
         console.log("Search completed", response);
-        this.results = response.data;
+        this.results = response.data.results;
       });
     },
   },
 };
+// Next need to display the raw data as just a title, then a title and a poster, then a title, poster, and button to add.
+// The button will run the create userMovie function for the user
+// ask peter if its worth keeping the movie database from here out (or rather the user_movie)
+// If database kept as is, button could run the following functions: check if the movie exists in 'movies', if it doesn't, run the createmovie function for said movie
+// Else just create user_movie
 </script>
 
 <template>
   <div class="user-movies-index">
     <h1>Your Top 10</h1>
-    <p>Your Favorite Movies: {{ this.user_movies }}</p>
+    <div v-for="user_movie in user_movies" v-bind:key="user_movie.id">
+      <p>{{ user_movie.movie.title }}</p>
+    </div>
   </div>
   <div class="movie-search">
     <h1>Search for Movies!</h1>
     <div>
       <label>Search:</label>
-      <form v-on:submit="movieSearch()"></form>
       <button @click="movieSearch">Search</button>
       <input type="text" v-model="searchText" />
-      <p>{{ this.results }}</p>
+      <div v-for="result in results" v-bind:key="result.id">
+        <p>{{ result.title }}</p>
+      </div>
     </div>
   </div>
 </template>
